32554a0667f616fc49740ae1dd4f06916d304ef3
wrightchin <mschincsk@gmail.com>
2022-10-27 21:39:54 +0800

    stable version

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -23,7 +23,7 @@ export default function Create() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form };
  
-   await fetch("http://localhost:5000/record/add", {
+   await fetch(`http://mern-demo-server-3-wrightchin-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/record/add`, {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -25,7 +25,7 @@ export default function RecordList() {
  // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`http://localhost:5000/record/`);
+     const response = await fetch(`http://mern-demo-server-3-wrightchin-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/record/`);
  
      if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
@@ -44,7 +44,7 @@ export default function RecordList() {
  
  // This method will delete a record
  async function deleteRecord(id) {
-   await fetch(`http://localhost:5000/${id}`, {
+   await fetch(`http://mern-demo-server-3-wrightchin-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/${id}`, {
      method: "DELETE"
    });
  
